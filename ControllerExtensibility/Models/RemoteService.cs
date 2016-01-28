de7fb0f6637df873712dbec03fe6343112e92e1e
srkneo <yadavmaheshing@gmail.com>
2016-01-28 16:56:17 +0530

    Max

---
+++ ControllerExtensibility/Models/RemoteService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ControllerExtensibility.Models
+{
+    public class RemoteService
+    {
+        public string GetRemoteData()
+        {
+            Thread.Sleep(5000);
+            return "Hello from the otherside of the world";
+        }
+
+        public async Task<string> GetRemoteDataAsync()
+        {
+            return await Task<string>.Factory.StartNew(() => {
+                Thread.Sleep(5000);
+                return "Hello from the otherside of the world";
+            });
+        }
+    }
+}
